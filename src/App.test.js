1ba43e15204ff6491a5afd0f95b2388b62518722
vasurajan <63305945+vasurajan@users.noreply.github.com>
2020-11-09 18:05:01 +0530

    Initial commit with our first test check app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+
+it('if everything works fine', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+
+  expect(div.innerHTML).toContain("Hi Scofield");
+})
+
